c73707455042e56996ef7cb59f3fc196ed3c7f3c
Justin Kolberg <amd.prophet@gmail.com>
2020-10-30 15:23:12 -0700

    another attempt
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- site-cookbooks/omnibus_sensu/recipes/fixes.rb
+++ site-cookbooks/omnibus_sensu/recipes/fixes.rb
@@ -8,20 +8,24 @@ debian_8 = node["platform"] == "debian" && Gem::Version.new(node["platform_versi
 
 if debian_8
   execute "ls /etc/apt/" do
+    compile_time true
     action :nothing
-  end.run_action
+  end.run_action(:run)
 
   execute "cat /etc/apt/sources.list" do
+    compile_time true
     action :nothing
-  end.run_action
+  end.run_action(:run)
 
   execute "ls /etc/apt/sources.list.d" do
+    compile_time true
     action :nothing
-  end.run_action
+  end.run_action(:run)
 
   execute "cat /etc/apt/sources.list.d/*" do
+    compile_time true
     action :nothing
-  end.run_action
+  end.run_action(:run)
 end
 
 if node["platform"] == "windows"
@@ -31,19 +35,19 @@ if node["platform"] == "windows"
   remote_file "c:/#{pkg_file}" do
     source "#{msys_repo}/x86_64/#{pkg_file}"
     action :nothing
-  end.run_action
+  end.run_action(:create)
 
   sig_file = "msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
   remote_file "c:/#{sig_file}" do
     source "#{msys_repo}/x86_64/#{sig_file}"
     action :nothing
-  end.run_action
+  end.run_action(:create)
 
   execute "pacman-key --verify c:/#{sig_file}" do
     action :nothing
-  end.run_action
+  end.run_action(:create)
 
   execute "pacman -U c:/#{pkg_file}" do
     action :nothing
-  end.run_action
+  end.run_action(:create)
 end
