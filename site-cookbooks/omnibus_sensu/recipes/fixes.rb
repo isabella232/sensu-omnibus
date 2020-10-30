7cbf0243d7ca86f8fc7eea52e06d358340add1ba
Justin Kolberg <amd.prophet@gmail.com>
2020-10-30 15:43:29 -0700

    another attempt
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- site-cookbooks/omnibus_sensu/recipes/fixes.rb
+++ site-cookbooks/omnibus_sensu/recipes/fixes.rb
@@ -4,30 +4,6 @@
 #
 # Copyright (c) 2020 Sensu, All Rights Reserved.
 
-debian_8 = node["platform"] == "debian" && Gem::Version.new(node["platform_version"]) == Gem::Version.new(8)
-
-if debian_8
-  execute "ls /etc/apt/" do
-    compile_time true
-    action :nothing
-  end.run_action(:run)
-
-  execute "cat /etc/apt/sources.list" do
-    compile_time true
-    action :nothing
-  end.run_action(:run)
-
-  execute "ls /etc/apt/sources.list.d" do
-    compile_time true
-    action :nothing
-  end.run_action(:run)
-
-  execute "cat /etc/apt/sources.list.d/*" do
-    compile_time true
-    action :nothing
-  end.run_action(:run)
-end
-
 if node["platform"] == "windows"
   msys_repo = "http://repo.msys2.org/msys"
 
@@ -45,9 +21,9 @@ if node["platform"] == "windows"
 
   execute "pacman-key --verify c:/#{sig_file}" do
     action :nothing
-  end.run_action(:create)
+  end.run_action(:run)
 
   execute "pacman -U c:/#{pkg_file}" do
     action :nothing
-  end.run_action(:create)
+  end.run_action(:run)
 end
