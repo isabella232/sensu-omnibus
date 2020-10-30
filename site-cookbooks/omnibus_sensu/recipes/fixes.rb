81a484f21d79849be2d3d3979633474d74ab4020
Justin Kolberg <amd.prophet@gmail.com>
2020-10-30 14:38:17 -0700

    run fixes at compile time
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- site-cookbooks/omnibus_sensu/recipes/fixes.rb
+++ site-cookbooks/omnibus_sensu/recipes/fixes.rb
@@ -7,15 +7,43 @@
 debian_8 = node["platform"] == "debian" && Gem::Version.new(node["platform_version"]) == Gem::Version.new(8)
 
 if debian_8
-  execute "ls /etc/apt/"
-  execute "cat /etc/apt/sources.list"
-  execute "ls /etc/apt/sources.list.d"
-  execute "cat /etc/apt/sources.list.d/*"
+  execute "ls /etc/apt/" do
+    action :nothing
+  end.run_action
+
+  execute "cat /etc/apt/sources.list" do
+    action :nothing
+  end.run_action
+
+  execute "ls /etc/apt/sources.list.d" do
+    action :nothing
+  end.run_action
+
+  execute "cat /etc/apt/sources.list.d/*" do
+    action :nothing
+  end.run_action
 end
 
 if node["platform"] == "windows"
-  execute "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
-  execute "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
-  execute "pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
-  execute "pacman -U msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
+  msys_repo = "http://repo.msys2.org/msys"
+
+  pkg_file = "msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
+  remote_file "c:/#{pkg_file}" do
+    source "#{msys_repo}/x86_64/#{pkg_file}"
+    action :nothing
+  end.run_action
+
+  sig_file = "msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
+  remote_file "c:/#{sig_file}" do
+    source "#{msys_repo}/x86_64/#{sig_file}"
+    action :nothing
+  end.run_action
+
+  execute "pacman-key --verify c:/#{sig_file}" do
+    action :nothing
+  end.run_action
+
+  execute "pacman -U c:/#{pkg_file}" do
+    action :nothing
+  end.run_action
 end
