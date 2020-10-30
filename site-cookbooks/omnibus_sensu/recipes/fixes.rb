069feb7de17a83eba340661d73f368fdf438e646
Justin Kolberg <amd.prophet@gmail.com>
2020-10-30 13:18:29 -0700

    fixes recipe should run before all others
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- site-cookbooks/omnibus_sensu/recipes/fixes.rb
+++ site-cookbooks/omnibus_sensu/recipes/fixes.rb
@@ -7,19 +7,15 @@
 debian_8 = node["platform"] == "debian" && Gem::Version.new(node["platform_version"]) == Gem::Version.new(8)
 
 if debian_8
-  execute "list_apt_dir" do
-    command("ls /etc/apt/")
-  end
+  execute "ls /etc/apt/"
+  execute "cat /etc/apt/sources.list"
+  execute "ls /etc/apt/sources.list.d"
+  execute "cat /etc/apt/sources.list.d/*"
+end
 
-  execute "show_sources_list" do
-    command("cat /etc/apt/sources.list")
-  end
-
-  execute "list_sources_dir" do
-    command("ls /etc/apt/sources.list.d")
-  end
-
-  execute "show_all_sources_list" do
-    command("cat /etc/apt/sources.list.d/*")
-  end
+if windows?
+  execute "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
+  execute "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
+  execute "pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
+  execute "pacman -U msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
 end
