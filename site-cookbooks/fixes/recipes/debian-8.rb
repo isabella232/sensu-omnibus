36d5ac5328ce364a3c868876fdb853f52a6d4be1
Justin Kolberg <amd.prophet@gmail.com>
2020-10-30 19:06:31 -0700

    update mingw cookbook, remove compile_time option from deb8 fixes
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- site-cookbooks/fixes/recipes/debian-8.rb
+++ site-cookbooks/fixes/recipes/debian-8.rb
@@ -5,21 +5,17 @@
 # Copyright (c) 2020 Sensu, All Rights Reserved.
 
 execute "ls /etc/apt/" do
-  compile_time true
   action :run
 end
 
 execute "cat /etc/apt/sources.list" do
-  compile_time true
   action :run
 end
 
 execute "ls /etc/apt/sources.list.d" do
-  compile_time true
   action :run
 end
 
 execute "cat /etc/apt/sources.list.d/*" do
-  compile_time true
   action :run
 end
